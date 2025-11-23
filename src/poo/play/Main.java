a4facc34263a7591d0154a3a742ab247cb4f59cb
gtoledo1916-arch <gtoledo1916@gmail.com>
2025-11-23 09:24:28 -0600

    Aprendiendo java while implementado

--- src/poo/play/Main.java
+++ src/poo/play/Main.java
@@ -7,44 +7,46 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int opcion;
         int totalPrioridad = 0;
 
+    do{
         System.out.println(" == Gestor de Tareas ===");
         System.out.println("1. Agregar tarea");
         System.out.println("2. Ver tareas (pendiente)");
         System.out.println("3. Salir");
 
         System.out.println("Selecciona una opción: ");
-        int opcion = sc.nextInt();
+        opcion = sc.nextInt();
         sc.nextLine(); //limpiar
 
-        // Validamos que este entre 1 y 3
-
-        if (opcion >= 1 && opcion <= 3) {
-            if (opcion == 1) {
+        switch (opcion){
+            case 1:
                 System.out.println("Nombre de la tarea: ");
-                String nombreTarea = sc.nextLine();
+                String nombre = sc.nextLine();
 
                 System.out.println("Prioridad (1 a 5): ");
                 int prioridad = sc.nextInt();
 
-                //Validación básica con operador lógico
-                boolean esValida = prioridad >= 1 && prioridad <= 5;
-                System.out.println("¿Prioridad es válida?" + esValida);
+               if (prioridad >= 1 && prioridad <= 5){
+                   totalPrioridad += prioridad;
+                   System.out.println("Tarea '" +  nombre + "' con prioridad " + prioridad + " agregada ");
+                   System.out.println("Total acumulado de proridades: " + totalPrioridad);
+               } else {
+                   System.out.println("Prioridad invalido");
+               }
+               break;
+            case 2:
+                System.out.println("Funcionalidad aún no implementada.");
+                break;
 
-                //Operación aritmética: suma prioridades
-                totalPrioridad += prioridad;
-
-                System.out.println("Tarea agregada: " + nombreTarea + "(Prioridad: " + prioridad + ")");
-                System.out.println("Suma total de Prioridades: " + totalPrioridad);
-            } else if (opcion == 2) {
-                System.out.println("Función para 'ver tareas' aun no implementada.");
-            } else {
-                System.out.println("!Hasta luego¡");
-            }
-        }else {
-            System.out.println("Opcion incorrecta");
+            case  3:
+                System.out.println("¡Hasta luego!");
+                break;
+            default:
+                System.out.println("Opcion invalida. Intenta de nuevo.");
         }
+    } while (opcion != 3);
 
         sc.close();
     }
