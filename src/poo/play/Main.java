26849e7f67e7b66b400266b5430faecde3d534ec
gtoledo1916-arch <gtoledo1916@gmail.com>
2025-11-27 16:32:37 -0600

    Correción de codigo y implementación de parametros

--- src/poo/play/Main.java
+++ src/poo/play/Main.java
@@ -5,49 +5,70 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+      Scanner sc = new Scanner(System.in);
+      int opcion;
+      int totalPrioridad = 0;
 
-        int opcion;
-        int totalPrioridad = 0;
+      do{
+          mostrarMenu();
+          opcion = leerOpcionMenu(sc);
+          totalPrioridad = ejecutarOpcion(opcion, sc, totalPrioridad);
+      } while (opcion != 3);
 
-    do{
-        System.out.println(" == Gestor de Tareas ===");
-        System.out.println("1. Agregar tarea");
-        System.out.println("2. Ver tareas (pendiente)");
+      sc.close();
+
+    }
+
+    public static void mostrarMenu() {
+        System.out.println("***Gestor de Tareas***");
+        System.out.println("1. Agregar Tarea");
+        System.out.println("2. Ver Tareas (Pendiente)");
         System.out.println("3. Salir");
+    }
 
-        System.out.println("Selecciona una opción: ");
-        opcion = sc.nextInt();
-        sc.nextLine(); //limpiar
+    public static int leerOpcionMenu(Scanner sc) {
+        System.out.println("Elije una opción: ");
+        return sc.nextInt();
+    }
 
-        switch (opcion){
+    public static int ejecutarOpcion(int opcion , Scanner sc , int totalPrioridad) {
+        sc.nextLine();
+
+        switch (opcion) {
             case 1:
-                System.out.println("Nombre de la tarea: ");
-                String nombre = sc.nextLine();
-
-                System.out.println("Prioridad (1 a 5): ");
-                int prioridad = sc.nextInt();
-
-               if (prioridad >= 1 && prioridad <= 5){
-                   totalPrioridad += prioridad;
-                   System.out.println("Tarea '" +  nombre + "' con prioridad " + prioridad + " agregada ");
-                   System.out.println("Total acumulado de proridades: " + totalPrioridad);
-               } else {
-                   System.out.println("Prioridad invalido");
-               }
+                totalPrioridad = agregarTarea(sc, totalPrioridad);
                 break;
+
             case 2:
-                System.out.println("Funcionalidad aún no implementada.");
+                System.out.println("Funcionalidad en desarrollo");
                 break;
 
-            case  3:
+            case 3:
                 System.out.println("¡Hasta luego!");
                 break;
             default:
-                System.out.println("Opcion invalida. Intenta de nuevo.");
+                System.out.println("Opcion incorrecta. Intente nuevamente");
         }
-    } while (opcion != 3);
+        return totalPrioridad;
+    }
 
-        sc.close();
+    public static int agregarTarea(Scanner sc, int totalPrioridad) {
+        System.out.println("Nombre de la tarea: ");
+        String nombre = sc.nextLine();
+
+        System.out.println("Prioridad (1 a 5): ");
+        int prioridad = sc.nextInt();
+        sc.nextLine();
+
+        if (prioridad >=1 && prioridad <= 5 ){
+            totalPrioridad += prioridad;
+            System.out.println("Tarea'" + nombre + "´ agregada con prioridad: " + prioridad);
+            System.out.println("Total acumulado de prioridades: " + totalPrioridad);
+        } else  {
+            System.out.println("Prioridad incorrecto. Intente nuevamente");
+        }
+
+        return totalPrioridad;
     }
 }
+
