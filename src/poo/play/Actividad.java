ed07ddee3e135906e98d9d63728f34f4a95e6ac7
gtoledo1916-arch <gtoledo1916@gmail.com>
2025-11-28 13:02:33 -0600

    implementación de herencia, polimorfismo y abstracción

---
+++ src/poo/play/Actividad.java
@@ -0,0 +1,15 @@
+package poo.play;
+
+public abstract class Actividad {
+    private  String nombre;
+
+    public Actividad(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public abstract void ejecutar();
+}
--- src/poo/play/Main.java
+++ src/poo/play/Main.java
@@ -8,11 +8,14 @@ public class Main {
       Scanner sc = new Scanner(System.in);
       int opcion;
       int totalPrioridad = 0;
+      Actividad ultimaTarea = null;
 
       do{
           mostrarMenu();
           opcion = leerOpcionMenu(sc);
-          totalPrioridad = ejecutarOpcion(opcion, sc, totalPrioridad);
+          Resultado resultado = ejecutarOpcion(opcion, sc, totalPrioridad, ultimaTarea);
+          totalPrioridad = resultado.totalPrioridad;
+          ultimaTarea = resultado.tarea;
       } while (opcion != 3);
 
       sc.close();
@@ -22,7 +25,7 @@ public class Main {
     public static void mostrarMenu() {
         System.out.println("***Gestor de Tareas***");
         System.out.println("1. Agregar Tarea");
-        System.out.println("2. Ver Tareas (Pendiente)");
+        System.out.println("2. Ver ultima tarea");
         System.out.println("3. Salir");
     }
 
@@ -31,28 +34,30 @@ public class Main {
         return sc.nextInt();
     }
 
-    public static int ejecutarOpcion(int opcion , Scanner sc , int totalPrioridad) {
-        sc.nextLine();
+    public static Resultado ejecutarOpcion(int opcion , Scanner sc , int totalPrioridad, Actividad ultimaTarea) {
+        sc.nextLine(); // limpiar buffer
 
         switch (opcion) {
             case 1:
-                totalPrioridad = agregarTarea(sc, totalPrioridad);
-                break;
+                return agregarTarea(sc, totalPrioridad);
 
             case 2:
-                System.out.println("Funcionalidad en desarrollo");
+                if (ultimaTarea != null){
+                    ultimaTarea.ejecutar(); //polimorfismo
+                } else {
+                    System.out.println("Tarea no existe");
+                }
                 break;
-
             case 3:
                 System.out.println("¡Hasta luego!");
                 break;
             default:
                 System.out.println("Opcion incorrecta. Intente nuevamente");
         }
-        return totalPrioridad;
+        return new Resultado(totalPrioridad, ultimaTarea);
     }
 
-    public static int agregarTarea(Scanner sc, int totalPrioridad) {
+    public static Resultado agregarTarea(Scanner sc, int totalPrioridad) {
         System.out.println("Nombre de la tarea: ");
         String nombre = sc.nextLine();
 
@@ -62,13 +67,26 @@ public class Main {
 
         if (prioridad >=1 && prioridad <= 5 ){
             totalPrioridad += prioridad;
+            Tarea nueva =  new Tarea(nombre, prioridad);
             System.out.println("Tarea'" + nombre + "´ agregada con prioridad: " + prioridad);
             System.out.println("Total acumulado de prioridades: " + totalPrioridad);
+            return new Resultado(totalPrioridad, nueva);
         } else  {
             System.out.println("Prioridad incorrecto. Intente nuevamente");
+            return new Resultado(totalPrioridad, null);
         }
+    }
 
-        return totalPrioridad;
+    // clase auxiliar para retornar múltiples valores
+    public static class Resultado {
+        int totalPrioridad;
+        Actividad tarea;
+
+
+        public  Resultado(int totalPrioridad, Actividad tarea){
+            this.totalPrioridad = totalPrioridad;
+            this.tarea = tarea;
+        }
     }
 }
 
